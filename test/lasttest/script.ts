35c9b8127c13564544262c24157c8c8464a5430f
hienfinity <hiendo2000@yahoo.de>
2026-05-29 19:22:19 +0200

    add const

--- test/lasttest/script.ts
+++ test/lasttest/script.ts
@@ -4,3 +4,26 @@ import { type Options } from "k6/options";
 // @ts-expect-error https://github.com/grafana/k6-jslib-testing
 import { expect } from "https://jslib.k6.io/k6-testing/0.6.1/index.js";
 
+const baseUrl = "https://localhost:3000";
+const restUrl = `${baseUrl}/rest/player`;
+const graphqlUrl = `${baseUrl}/graphql`;
+const tokenUrl = `${baseUrl}/auth/token`;
+const dbPopulateUrl = `${baseUrl}/dev/db_populate`;
+
+const ids = [1, 20, 30, 40, 50, 60];
+const usernames = ["a", "e", "r"];
+const emails = [
+  "player1@example.com",
+  "player2@example.com",
+  "player3@example.com",
+];
+const playerClasses = ["WARRIOR", "MAGE", "ROGUE"];
+
+const tlsDir = "../../src/config/resources/tls";
+const cert = open(`${tlsDir}/certificate.crt`);
+const key = open(`${tlsDir}/key.pem`);
+
+const rampUpDuration = "5s";
+const steadyDuration = "20s";
+const rampDownDuration = "5s";
+
